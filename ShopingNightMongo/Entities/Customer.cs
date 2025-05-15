d10c87d781d10982458b1fb8f9132f306aa1238f
Mehmet <mehmetzahir5834@gmail.com>
2025-05-15 20:12:22 +0300

    Ok

---
+++ ShopingNightMongo/Entities/Customer.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace ShopingNightMongo.Entities
+{
+    public class Customer
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerSurname { get; set; }
+    }
+}
